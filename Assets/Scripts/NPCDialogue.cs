e2baf099a274f1872e657036fd0de0d86ba2a94c
JoeM <joe.moloney496@gmail.com>
2021-03-08 11:44:59 +0000

    Cleaned Up NPCDialogue For NPC Array

--- Assets/Scripts/NPCDialogue.cs
+++ Assets/Scripts/NPCDialogue.cs
@@ -6,13 +6,57 @@ using UnityEngine;
 public class NPCDialogue //Objects Attributes
 {
     //Attributes found in Json file relating to NPC Dialogues
-    public string Name, Description, Building, Pastry,
-        Introduction, Acquaintance1, Acquaintance2, Friend1,
-        Friend2, Friend3, Election, BestFriend1,
-        BestFriend2, BuildingQ, BuildingComplete, NPC,
-        Job, Quest, QuestComplete;
+    public string Name;
+    public string Description;
+    public string Building;
+    public string Pastry;
+    public string Introduction;
+    public string Acquaintance1;
+    public string Acquaintance2;
+    public string Friend1;
+    public string Friend2;
+    public string Friend3;
+    public string Election;
+    public string BestFriend1;
+    public string BestFriend2;
+    public string BuildingQ;
+    public string BuildingComplete;
+    public string NPC;
+    public string Job;
+    public string Quest;
+    public string QuestComplete;
 
-    public static bool firstEncounter = true;
+    public int rLevel = 0; //Create attribute storing Players Relationship Level to the NPC
+
+    //public static string CheckRLevel(string name, Dialogues dialogues)
+    //{
+    //    string dialogue = null;
+    //    foreach(NPCDialogue npc in npcDialogues.dialogues)
+    //    {
+    //        if (name.Equals(npc.Name))
+    //        {
+    //            Debug.Log($"Name: {npc.Name}, Relationship Level: {npc.rLevel}");
+    //            switch (npc.rLevel) //Selected dialogue based on relationship level [ >=5 cases, compiler should convert to JumpTable @ compile time?]
+    //            {
+    //                case 0: { dialogue = npc.Introduction; break; }
+    //                case 1: { dialogue = npc.Acquaintance1; break; }
+    //                case 2: { dialogue = npc.Acquaintance2; break; }
+    //                case 3: { dialogue = npc.Friend1; break; }
+    //                case 4: { dialogue = npc.Friend2; break; }
+    //                case 5: { dialogue = npc.Friend3; break; }
+    //                case 6: { dialogue = npc.BestFriend1; break; }
+    //                case 7: { dialogue = npc.BestFriend2; break; }
+    //                case 8: { dialogue = npc.BuildingQ; break; }
+    //                case 9: { dialogue = npc.BuildingComplete; break; }
+    //                case 10: { dialogue = npc.Quest; break; }
+    //                case 11: { dialogue = npc.QuestComplete; break; }
+    //                default: { dialogue = "End Of Dialogue Switch"; npc.rLevel = 0; break; }
+    //            }
+    //        }
+    //        break;
+    //    }
+    //    return dialogue;
+    //}
 
     public override string ToString()
     {
@@ -25,6 +69,6 @@ public class NPCDialogue //Objects Attributes
 
     public string NPCDetails()
     { 
-        return string.Format($"NPC Name: {Name}, \nNPC Description: {Description}, \nHome Building: {Building}, \nFavorite Pastry: {Pastry}, \nFirst Encounter?: {firstEncounter}");
+        return string.Format($"NPC Name: {Name}, \nNPC Description: {Description}, \nHome Building: {Building}, \nFavorite Pastry: {Pastry}");
     }
 }
