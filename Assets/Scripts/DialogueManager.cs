9c622428e44f682150f0b86b3917d1ae8e6dab71
Cathal <47002668+cobby363@users.noreply.github.com>
2021-03-18 10:58:20 +0000

    Update DialogueManager.cs

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+<<<<<<< HEAD
 using System.Linq;
+=======
+>>>>>>> parent of aa1fc90 (fixing the last of the quest diag box bugs)
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -241,10 +244,8 @@ public class DialogueManager : MonoBehaviour
                             }
                             else
                                 NPCReplyText = "Invalid text";
-                            if (!StringIncludesLetters(QuestManager.Instance.ActiveQuests.text))
-                            {
-                                QuestManager.Instance.ActiveQuests.text = QuestManager.Instance.ActiveQuests.text.Replace("\n", string.Empty);
-                            }
+
+                            QuestManager.Instance.ActiveQuests.text = QuestManager.Instance.ActiveQuests.text.Replace("\n", string.Empty);
                         }
                         if (QuestManager.Instance.ActiveQuests.text == string.Empty)
                             QuestManager.Instance.QuestBox.SetActive(false);
@@ -268,12 +269,6 @@ public class DialogueManager : MonoBehaviour
                 break;//stop the foreach loop continuing after we've found our target npc
             }
         }
-    }
-
-    private static bool StringIncludesLetters(String str)
-    {
-        return QuestManager.Instance.ActiveQuests.text.Any(x => char.IsLetter(x));
-        //return Regex.IsMatch(str, @"^[a-zA-Z]+$");
     }
 
     public string GetRelationshipLevel(string NPCName)
